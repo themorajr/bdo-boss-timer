ac98108c3cd2d0449c7ad85439d94462d24534ec
พี่ปลาวาฬ <42457039+themorajr@users.noreply.github.com>
2023-01-22 06:02:11 +0700

    Update alert.py
    Delete comments

--- alert.py
+++ alert.py
@@ -17,46 +17,6 @@ bot = discord.Client(intents=discord.Intents.all())
 logger = logging.getLogger(__name__)
 
 @bot.event
-# async def on_ready():
-#     channel_id = 1065650881036550234 # hardcoded channel ID
-#     for timer in boss_timers:
-#         now = datetime.now()
-#         time = datetime.strptime(timer["time"], "%H:%M").time()
-#         target_day = now.replace(hour=time.hour, minute=time.minute, second=0, microsecond=0)
-#         if datetime.now().weekday() == weekday_dict[timer["day_of_week"]]:
-#             if now.time() > time:
-#                     target_day += timedelta(days=1)
-#         else:
-#             target_day += timedelta(days=(weekday_dict[timer["day_of_week"]] - now.weekday()) % 7)
-#         delay = (target_day - datetime.now()).total_seconds()
-#         bot.loop.create_task(alert(timer["name"], delay, channel_id))
-#         await asyncio.sleep(1)
-
-
-# async def alert(boss_name: str, delay: int, channel_id: int) -> None:
-#     channel_id = 1065650881036550234 # hardcoded channel ID
-#     messages = [f"@everyone 30 minutes left before {boss_name} spawns!",
-#                 f"@everyone 15 minutes left before {boss_name} spawns!",
-#                 f"@everyone 5 minutes left before {boss_name} spawns!",
-#                 f"@everyone {boss_name} has spawned!"]
-
-#     delay = delay - 1800  # starts sending messages 30 minutes before spawn
-#     delay_between_messages = [1800, 900, 900, 300]
-#     for i in range(len(messages)):
-#         await asyncio.sleep(delay)
-#         try:
-#             await bot.get_channel(channel_id).send(messages[i])
-#         except Exception as e:
-#             print("Error occured: ", e)
-#         delay += delay_between_messages[i]
-
-
-# async def show_table(channel):
-#     table = PrettyTable()
-#     table.field_names = ["Boss Name", "Day of Week", "Time"]
-#     for boss in boss_timers:
-#         table.add_row([boss["name"], boss["day_of_week"], boss["time"]])
-#     await channel.send("```"+table.get_string()+"```")
 
 async def on_ready():
     for timer in boss_timers:
@@ -103,34 +63,6 @@ async def alert(boss_name: str, target_time: datetime, channel_id: int) -> None:
                 logger.error(f"Error occured: {e}")
                 return
         await asyncio.sleep(60)
-
-
-# async def alert(boss_name: str, delay: int, channel_id: int) -> None:
-#     messages = [f"@everyone 3 minutes left before {boss_name} spawns!",
-#                 f"@everyone 2 minutes left before {boss_name} spawns!",
-#                 f"@everyone 1 minute left before {boss_name} spawns!",
-#                 f"@everyone {boss_name} has spawned!"]
-
-#     delay = delay - 180  # starts sending messages 3 minutes before spawn
-#     for i in range(len(messages) -1):
-#         await asyncio.sleep(60 * (3-i))
-#         try:
-#             channel = bot.get_channel(channel_id)
-#             if channel is None:
-#                 print(f'Error: Could not find channel with ID {channel_id}')
-#                 return
-#             await channel.send(messages[i])
-#         except Exception as e:
-#             print("Error occured: ", e)
-#     await asyncio.sleep(delay)
-#     try:
-#         channel = bot.get_channel(channel_id)
-#         if channel is None:
-#             print(f'Error: Could not find channel with ID {channel_id}')
-#             return
-#         await channel.send(messages[-1])
-#     except Exception as e:
-#         print("Error occured: ", e)
 
 async def show_table(channel):
     table = PrettyTable()
